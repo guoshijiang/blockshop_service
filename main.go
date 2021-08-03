63e3547a2de844e0780b33d882a324f6e82cd9bb
郭世江 <bhex@guoshijiangs-MacBook-Pro.local>
2021-08-04 00:03:28 +0800

    fix

--- main.go
+++ main.go
@@ -5,9 +5,26 @@ import (
   _ "blockshop/routers"
   _ "blockshop/session"
   "github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 )
 
 func main() {
+	//orm.Debug = true
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	beego.BConfig.WebConfig.Session.SessionOn = true             //开启Session模块
+	beego.BConfig.WebConfig.Session.SessionGCMaxLifetime = 86400 //设置Session有效期,单位秒
+	beego.BConfig.WebConfig.Session.SessionCookieLifeTime = 86400
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
 	beego.Run()
 }
+
 
